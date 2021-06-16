a385666fe6194673a3a30c0ae0a7dacf24306a43
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-06-16 16:28:11 +0100

    Added currency and percentage

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click", function () {
   let billAmount = document.getElementById("bill-amount").value;
   let tipPercentage = document.getElementById("tip-percentage").value;
-  let tipAmount = tipPercentage > 0 ? (billAmount / tipPercentage) : 0;
+  let tipAmount = tipPercentage > 0 ? billAmount / tipPercentage : 0;
   let numberOfPeople = document.getElementById("number-people").value;
 
   let billTotal = parseFloat(billAmount) + parseFloat(tipAmount);
   let totalPerPerson = document.getElementById("total-per-person");
-  let finalAmount = new Intl.NumberFormat("en-IN", { style: 'currency', currency: 'GBP' }).format(
-    billTotal / numberOfPeople
-  );
-    totalPerPerson.innerHTML = finalAmount;
+  let finalAmount = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "GBP",
+  }).format(billTotal / numberOfPeople);
+  totalPerPerson.innerHTML = finalAmount;
 });
